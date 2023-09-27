34f4b761a250ff3aae679bea1255f29deb5800d7
el-sam <samurai.h@yahoo.com>
2023-09-27 20:12:33 +0800

    add value for the textConent of node of h1 element.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // Write your code here!
 const main = document.getElementById('main');
 main.remove();
+
+//  Create a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement('h1');
+
+//  Add value for id element in h1 node
+newHeader.id = "victory";
+
+// inser the text for h1 node
+newHeader.textContent = "YOUR-NAME is the champion"
+
+// Get node for where the new element to be added to, then add.
+const body = document.getElementsByTagName('body')[0];
+
+body.append(newHeader);
